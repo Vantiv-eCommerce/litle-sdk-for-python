13dd370933a26ee82b6a21ec6bfc1ccb1fe38e5f
Kush, Patel <kpatel@litle.com>
2012-05-31 09:44:22 -0400

    certTest2 complete, all working, passing tests. Changed setuptest to run against sandbox, not cert

--- test/certification/SetupTest.py
+++ test/certification/SetupTest.py
@@ -4,12 +4,10 @@ sys.path.append(lib_path)
 
 from litleOnlineRequest import *
 
-
 config = Configuration()
 config.setPassword('certpass')
 config.setUser('PHXMLTEST')
 config.setMerchantId('101')
 config.setReportGroup('Planets')
-#config.setProxy('smoothproxy:8080')
-config.setUrl('Cert')
-
+config.setProxy('smoothproxy:8080')
+config.setUrl('Sandbox')
