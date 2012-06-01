d5a4aa075d0f93e2a326d35b437f3a7782757493
Rushi <rganmukh@litle.com>
2012-06-01 15:59:57 -0400

    override config settings

--- lib/Communications.py
+++ lib/Communications.py
@@ -2,22 +2,28 @@ import urllib2
 
 class Communications:
     def __init__(self,Configuration):
-        self.timeout = Configuration.getTimeout()
-        self.proxy = Configuration.getProxy()
-        self.url = Configuration.getUrl()
+        self.Timeout = Configuration.getTimeout()
+        self.Proxy = Configuration.getProxy()
+        self.Url = Configuration.getUrl()
         
-    def http_post(self,post_data):
-       
-        req = urllib2.Request(url= self.url, data=post_data)
+    def http_post(self,post_data,url=None,proxy=None,timeout=None):
+        if (url != None):
+            self.Url = url
+        if (proxy != None):
+            self.Proxy = proxy
+        if (timeout != None):
+            self.Timeout = timeout
+            
+        req = urllib2.Request(url= self.Url, data=post_data)
         req.add_header('Content-type', 'text/xml')
             
         try: 
-            if (self.proxy != None) :  
-                proxy_handler = urllib2.ProxyHandler({'https': self.proxy}) 
+            if (self.Proxy != None) :  
+                proxy_handler = urllib2.ProxyHandler({'https': self.Proxy}) 
                 opener = urllib2.build_opener(proxy_handler)
-                response = opener.open(req, timeout=self.timeout)
+                response = opener.open(req, timeout=self.Timeout)
             else:
-                response = urllib2.urlopen(req, timeout=self.timeout)
+                response = urllib2.urlopen(req, timeout=self.Timeout)
                 
         except:
             raise Exception("Error with Https Request, Please Check Proxy and Url configuration")
--- lib/litleOnlineRequest.py
+++ lib/litleOnlineRequest.py
@@ -4,11 +4,11 @@ class litleOnlineRequest:
         
     def __init__(self,Configuration):
         self.Configuration = Configuration
-        self.merchantId = Configuration.getMerchantId()
-        self.user = Configuration.getUser()
-        self.password = Configuration.getPassword()
-        self.version = Configuration.getVersion()
-        self.reportGroup = Configuration.getReportGroup()
+        self.MerchantId = Configuration.getMerchantId()
+        self.User = Configuration.getUser()
+        self.Password = Configuration.getPassword()
+        self.Version = Configuration.getVersion()
+        self.ReportGroup = Configuration.getReportGroup()
         
     def _litleToXml(self,litleOnline):
         try:
@@ -18,23 +18,34 @@ class litleOnlineRequest:
         # TODO 
         temp= temp.replace('ns1:','')
         return temp.replace(':ns1','')
-    
-    def litleXmlMapper(self,transaction):
-        litleOnline = self._createTxn(transaction)
+        
+    def litleXmlMapper(self,transaction,user = None, password = None, version = None,  merchantId = None,reportGroup = None, timeout = None,url = None, proxy = None):
+        if (user != None):
+            self.User = user
+        if (password != None):
+            self.Password = password
+        if (version != None):
+            self.Version = version
+        if (merchantId != None):
+            self.MerchantId = merchantId
+        if (reportGroup != None):
+            self.ReportGroup = reportGroup
+            
+        litleOnline = self._createTxn(transaction)        
         comm = Communications(self.Configuration)
-        responseXml = comm.http_post(self._litleToXml(litleOnline))
+        responseXml = comm.http_post(self._litleToXml(litleOnline),url=url,proxy=proxy,timeout=timeout)
         return self._processResponse(responseXml)
     
     def _createTxn(self,transaction):
         litleOnline = litleXmlFields.litleOnlineRequest()
-        litleOnline.merchantId = self.merchantId
-        litleOnline.version = self.version
+        litleOnline.merchantId = self.MerchantId
+        litleOnline.version = self.Version
 
         authentication = litleXmlFields.authentication()
-        authentication.user = self.user
-        authentication.password =  self.password 
+        authentication.user = self.User
+        authentication.password =  self.Password 
         litleOnline.authentication = authentication
-        transaction.reportGroup = self.reportGroup
+        transaction.reportGroup = self.ReportGroup
         litleOnline.transaction = transaction
         return litleOnline
     
