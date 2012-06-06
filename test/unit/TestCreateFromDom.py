8a1f0ad921af2f6136f4614233bd1d9148bd1635
Rushi <rganmukh@litle.com>
2012-06-06 14:32:10 -0400

    test createFromDom Method

--- test/unit/TestCreateFromDom.py
+++ test/unit/TestCreateFromDom.py
@@ -13,11 +13,18 @@ class TestHandleExtraField(unittest.TestCase):
         xml_object = litleXmlFields.CreateFromDocument(xml_text)
         self.assertEquals("Approved",xml_object.transactionResponse.message)
         
-    def test_simple_ExtraField_EmbeddedExtraField(self):
-        xml_text="<litleOnlineResponse version='8.13' response='0' message='Valid Format' xmlns='http://www.litle.com/schema'><registerTokenResponse id='' reportGroup='DefaultReportGroup' customerId=''><litleTxnId>057484783403434000</litleTxnId><orderId>12344</orderId><response>000</response><responseTime>2012-06-05T16:36:39</responseTime><message>Approved</message><authCode><extraField>extra</extraField></authCode></registerTokenResponse></litleOnlineResponse>"
+    def test_simple_EmbeddedField(self):
+        xml_text="<litleOnlineResponse version='8.13' response='0' message='Valid Format' xmlns='http://www.litle.com/schema'><authorizationResponse id='' reportGroup='DefaultReportGroup' customerId=''><litleTxnId>057484783403434000</litleTxnId><orderId>12344</orderId><response>000</response><responseTime>2012-06-05T16:36:39</responseTime><message>Approved</message><tokenResponse><litleToken>4242424242424242</litleToken><tokenResponseCode>111</tokenResponseCode><tokenMessage>Message</tokenMessage><bin>bin</bin></tokenResponse></authorizationResponse></litleOnlineResponse>"
         xml_object = litleXmlFields.CreateFromDocument(xml_text)
-        self.assertEquals("Approved",xml_object.transactionResponse.message)
+        self.assertEquals("bin",xml_object.transactionResponse.tokenResponse.bin)
+        self.assertEquals("Message",xml_object.transactionResponse.tokenResponse.tokenMessage)
 
+    def test_simple_ExtraEmbeddedField(self):
+        xml_text="<litleOnlineResponse version='8.13' response='0' message='Valid Format' xmlns='http://www.litle.com/schema'><authorizationResponse id='' reportGroup='DefaultReportGroup' customerId=''><litleTxnId>057484783403434000</litleTxnId><orderId>12344</orderId><response>000</response><responseTime>2012-06-05T16:36:39</responseTime><message>Approved</message><tokenResponse><litleToken>4242424242424242</litleToken><tokenResponseCode>111</tokenResponseCode><tokenMessage>Message</tokenMessage><bin>bin</bin><extra>extra</extra></tokenResponse></authorizationResponse></litleOnlineResponse>"
+        xml_object = litleXmlFields.CreateFromDocument(xml_text)
+        self.assertEquals("bin",xml_object.transactionResponse.tokenResponse.bin)
+        self.assertEquals("Message",xml_object.transactionResponse.tokenResponse.tokenMessage)
+        
 def suite():
     suite = unittest.TestSuite()
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSale)
