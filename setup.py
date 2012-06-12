202ce4ca61433c662fe24f1343d313bc1d708a10
Rushi <rganmukh@litle.com>
2012-06-12 17:12:30 -0400

    Setup.py file

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+setup(name='LitleSdkPython',
+      version='8.13.0',
+      description='Litle & Co. SDK for Python',
+      author='Litle & Co',
+      author_email='SDKSupport@litle.com',
+      url='http://www.litle.com/developers',
+      packages=['litleSdkPython'],
+      data_files=[("litleSdkPython",["LICENSE","README.md"])],
+      install_requires=[
+                        'PyXB'],
+      dependency_links =[
+                         'http://pypi.python.org/packages/source/P/PyXB/PyXB-full-1.1.3.tar.gz#md5=62f4e37baa59d9fc9df5f7186e7d63a2'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Environment :: MacOS X'
+          'Environment :: Plugins'
+          'Environment :: Win32 (MS Windows)'
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent'
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Office/Business :: Financial',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+     )
