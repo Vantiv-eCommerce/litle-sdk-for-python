8619d24c60118b30ddfc5b0aaf1881d94376ea77
Litle & Co <SDKSupport@litle.com>
2014-06-27 10:08:24 -0400

    Fixing PyXB version.

--- setup.py
+++ setup.py
@@ -10,11 +10,8 @@ setup(name='LitleSdkPython',
       url='http://www.litle.com/developers',
       packages=['litleSdkPython'],
       install_requires=[
-                        'PyXB',
-                        'paramiko'],
-      dependency_links =[
-                         'https://pypi.python.org/packages/source/P/PyXB/PyXB-1.1.5.tar.gz#md5=95ce5cd2ad7abfead042896f88d25eea',
-                         'https://pypi.python.org/packages/source/p/paramiko/paramiko-1.14.0.tar.gz#md5=e26324fd398af68ad506fe98853835c3'],
+                        'PyXB==1.1.5',
+                        'paramiko==1.14.0'],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
