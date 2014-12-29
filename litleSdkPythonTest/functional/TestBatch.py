b41292b3d232e70ca2c917c437cf8a9f7911e358
Greg, Dake <gdake@litle.com>
2014-12-29 14:47:38 -0500

    Separating out TCP based batch tests from sFTP based batch tests

--- litleSdkPythonTest/functional/TestBatch.py
+++ litleSdkPythonTest/functional/TestBatch.py
@@ -36,40 +36,8 @@ class TestBatch(unittest.TestCase):
         self.merchantId = '0180'
 
 
-    def testSendToLitle_WithFileConfig(self):
-        requestFileName = "litleSdk-testBatchFile-fileConfig.xml"
-        request = litleBatchFileRequest(requestFileName)
-        self.assertTrue(os.path.exists(request.requestFile.name))
-        configFromFile = request.config
-        self.assertEqual('prelive.litle.com', configFromFile.batchHost)
-        self.assertEqual('15000', configFromFile.batchPort)
-        self.prepareTestRequest(request)
-        response = request.sendRequestTCP()
-        self.assertPythonApi(request, response)
-        requestDir = configFromFile.batchRequestFolder
-        responseDir = configFromFile.batchResponseFolder
-        self.assertGeneratedFiles(requestDir, responseDir, requestFileName, request)
-
-    def testSendToLitle_WithConfigOverrides(self):
-        requestDir = tempfile.gettempdir() + '/' + 'request'
-        responseDir = tempfile.gettempdir() + '/' + 'response'
-
-        configOverrides = Configuration()
-        configOverrides.batchHost = 'prelive.litle.com'
-        configOverrides.batchPort = '15000'
-        configOverrides.batchRequestFolder = requestDir
-        configOverrides.batchResponseFolder = responseDir
-
-        requestFileName = "litleSdk-testBatchFile-fileConfig.xml"
-        request = litleBatchFileRequest(requestFileName, configOverrides)
-        self.assertTrue(os.path.exists(request.requestFile.name))
-        self.prepareTestRequest(request)
-        response = request.sendRequestTCP()
-        self.assertPythonApi(request, response)
-        self.assertGeneratedFiles(requestDir, responseDir, requestFileName, request)
-
     def testSendToLitleSFTP_WithPreviouslyCreatedFile(self):
-        requestFileName = "litleSdk-testBatchFile-fileConfig.xml"
+        requestFileName = "litleSdk-testBatchFile-testSendToLitleSFTP_WithPreviouslyCreatedFile.xml"
         request = litleBatchFileRequest(requestFileName)
         requestFile = request.requestFile.name
         self.assertTrue(os.path.exists(requestFile))
@@ -88,7 +56,7 @@ class TestBatch(unittest.TestCase):
         self.assertGeneratedFiles(requestDir, responseDir, requestFileName, request2)
 
     def testSendOnlyToLitleSFTP_WithPreviouslyCreatedFile(self):
-        requestFileName = "litleSdk-testBatchFile-fileConfig.xml"
+        requestFileName = "litleSdk-testBatchFile-testSendOnlyToLitleSFTP_WithPreviouslyCreatedFile.xml"
         request = litleBatchFileRequest(requestFileName)
         requestFile = request.requestFile.name
         self.assertTrue(os.path.exists(requestFile))
@@ -122,13 +90,14 @@ class TestBatch(unittest.TestCase):
         self.assertGeneratedFiles(newRequestDir, responseDir, newRequestFileName, request3)
 
     def testSendToLitleSFTP_WithFileConfig(self):
-        requestFileName = "litleSdk-testBatchFile-fileConfig.xml"
+        requestFileName = "litleSdk-testBatchFile-testSendToLitleSFTP_WithFileConfig.xml"
         request = litleBatchFileRequest(requestFileName)
         requestFile = request.requestFile.name
         self.assertTrue(os.path.exists(requestFile))
         configFromFile = request.config
         self.assertEqual('prelive.litle.com', configFromFile.batchHost)
         self.assertEqual('15000', configFromFile.batchPort)
+        self.assertEquals('True', configFromFile.printXml)
         requestDir = configFromFile.batchRequestFolder
         responseDir = configFromFile.batchResponseFolder
 
@@ -148,12 +117,14 @@ class TestBatch(unittest.TestCase):
         configOverrides.sftpTimeout = '720000'
         configOverrides.batchRequestFolder = requestDir
         configOverrides.batchResponseFolder = responseDir
+        configOverrides.printXml = True
 
-        requestFileName = "litleSdk-testBatchFile-configOverridesSFTP.xml"
+        requestFileName = "litleSdk-testBatchFile-testSendToLitleSFTP_WithConfigOverrides.xml"
         request = litleBatchFileRequest(requestFileName, configOverrides)
 
         self.assertTrue(os.path.exists(request.requestFile.name))
-
+        self.assertTrue(request.config.printXml)
+        
         self.prepareTestRequest(request)
 
         response = request.sendRequestSFTP()
@@ -161,337 +132,6 @@ class TestBatch(unittest.TestCase):
         self.assertPythonApi(request, response)
         self.assertGeneratedFiles(requestDir, responseDir, requestFileName, request)
 
-    def testMechantBatchAndProcess(self):
-        requestFileName = "litleSdk-testBatchFile-MECHA.xml"
-        request = litleBatchFileRequest(requestFileName)
-
-        configFromFile = request.config
-        self.assertEqual('prelive.litle.com', configFromFile.batchHost)
-        self.assertEqual('15000', configFromFile.batchPort)
-
-        batch = request.createBatch()
-
-        #card
-        card = litleXmlFields.cardType()
-        card.number = '4100000000000001'
-        card.expDate = '1210'
-        card.type = 'VI'
-
-        #echeck
-        echeck = litleXmlFields.echeck()
-        echeck.accNum = '1234567890'
-        echeck.accType = 'Checking'
-        echeck.routingNum = '123456789'
-        echeck.checkNum = '123455'
-
-        #billto address
-        contact = litleXmlFields.contact()
-        contact.name = 'Bob'
-        contact.city = 'lowell'
-        contact.state = 'MA'
-        contact.email = 'Bob@litle.com'
-
-        #auth
-        auth = litleXmlFields.authorization()
-        auth.reportGroup = 'Planets'
-        auth.orderId = '12344'
-        auth.amount = 106
-        auth.orderSource = 'ecommerce'
-        auth.card = card
-
-        batch.addTransaction(auth)
-
-        sale = litleXmlFields.sale()
-        sale.reportGroup = 'Planets'
-        sale.orderId = '12344'
-        sale.amount = 6000
-        sale.orderSource = 'ecommerce'
-        sale.card = card
-
-        batch.addTransaction(sale)
-
-        credit = litleXmlFields.credit()
-        credit.reportGroup = 'Planets'
-        credit.orderId = '12344'
-        credit.amount = 106
-        credit.orderSource = 'ecommerce'
-        credit.card = card
-
-        batch.addTransaction(credit)
-
-        authReversal = litleXmlFields.authReversal()
-        authReversal.reportGroup = 'Planets'
-        authReversal.litleTxnId = 12345678000
-        authReversal.amount = 106
-        authReversal.payPalNotes = 'Notes'
-
-        batch.addTransaction(authReversal)
-
-        registerTokenRequestType = litleXmlFields.registerTokenRequest()
-        registerTokenRequestType.reportGroup = 'Planets'
-        registerTokenRequestType.orderId = '12344'
-        registerTokenRequestType.accountNumber = '1233456789103801'
-
-        batch.addTransaction(registerTokenRequestType)
-
-        cardValidationNumOnToken = litleXmlFields.updateCardValidationNumOnToken()
-        cardValidationNumOnToken.reportGroup = 'Planets'
-        cardValidationNumOnToken.id = '12345'
-        cardValidationNumOnToken.customerId = '0987'
-        cardValidationNumOnToken.orderId = '12344'
-        cardValidationNumOnToken.litleToken = '1233456789103801'
-        cardValidationNumOnToken.cardValidationNum = '123'
-
-        batch.addTransaction(cardValidationNumOnToken)
-
-        forceCapture = litleXmlFields.forceCapture()
-        forceCapture.reportGroup = 'Planets'
-        forceCapture.id = '123456'
-        forceCapture.orderId = '12344'
-        forceCapture.amount = 106
-        forceCapture.orderSource = 'ecommerce'
-        forceCapture.card = card
-
-        batch.addTransaction(forceCapture)
-
-        capture = litleXmlFields.capture()
-        capture.reportGroup = 'Planets'
-        capture.litleTxnId = 123456000
-        capture.amount = 106
-
-        batch.addTransaction(capture)
-
-        captureGivenAuth = litleXmlFields.captureGivenAuth()
-        captureGivenAuth.reportGroup = 'Planets'
-        captureGivenAuth.orderId = '12344'
-        captureGivenAuth.amount = 106
-        authInformation = litleXmlFields.authInformation()
-        authInformation.authDate = pyxb.binding.datatypes.date(2002, 10, 9)
-        authInformation.authAmount = 12345
-        authInformation.authCode = '543216'
-        captureGivenAuth.authInformation = authInformation
-        captureGivenAuth.orderSource = 'ecommerce'
-        captureGivenAuth.card = card
-
-        batch.addTransaction(captureGivenAuth)
-
-        echeckVerification = litleXmlFields.echeckVerification()
-        echeckVerification.reportGroup = 'Planets'
-        echeckVerification.amount = 123456
-        echeckVerification.orderId = '12345'
-        echeckVerification.orderSource = 'ecommerce'
-        echeckVerification.billToAddress = contact
-        echeckVerification.echeckOrEcheckToken = echeck
-
-        batch.addTransaction(echeckVerification)
-
-        echeckCredit = litleXmlFields.echeckCredit()
-        echeckCredit.reportGroup = 'Planets'
-        echeckCredit.litleTxnId = 1234567890
-        echeckCredit.amount = 12
-
-        batch.addTransaction(echeckCredit)
-
-        echeckRedeposit = litleXmlFields.echeckRedeposit()
-        echeckRedeposit.reportGroup = 'Planets'
-        echeckRedeposit.litleTxnId = 124321341412
-
-        batch.addTransaction(echeckRedeposit)
-
-        echeckSale = litleXmlFields.echeckSale()
-        echeckSale.reportGroup = 'Planets'
-        echeckSale.amount = 123456
-        echeckSale.orderId = '12345'
-        echeckSale.orderSource = 'ecommerce'
-        echeckSale.billToAddress = contact
-        echeckSale.echeckOrEcheckToken = echeck
-        echeckSale.verify = True
-
-        batch.addTransaction(echeckSale)
-
-        transactionCount = batch.numOfTxn
-
-        fileResponse = request.sendRequestTCP()
-        batchResponse = fileResponse.getNextBatchResponse()
-
-        txns = 0
-
-        nextTransaction = True
-        while nextTransaction:
-            try:
-                batchResponse.getNextTransaction()
-                txns += 1
-            except:
-                nextTransaction = False
-
-        self.assertEqual(transactionCount, txns)
-
-    def testGiftCardTransactions(self):
-        requestFileName = "litleSdk-testBatchFile-GIFTCARD.xml"
-        request = litleBatchFileRequest(requestFileName)
-        configFromFile = request.config
-        self.assertEqual('prelive.litle.com', configFromFile.batchHost)
-        self.assertEqual('15000', configFromFile.batchPort)
-        batch = request.createBatch()
-
-        giftCard = litleXmlFields.cardType()
-        giftCard.type = 'GC'
-        giftCard.expDate = '1218'
-        giftCard.number = '4100000000000001'
-
-        activate = litleXmlFields.activate()
-        activate.reportGroup = 'Planets'
-        activate.orderSource = 'ecommerce'
-        activate.amount = 100
-        activate.orderId = 'abc'
-        activate.card = giftCard
-
-        batch.addTransaction(activate)
-
-        deactivate = litleXmlFields.deactivate()
-        deactivate.reportGroup = 'Planets'
-        deactivate.orderId = 'def'
-        deactivate.orderSource = 'ecommerce'
-        deactivate.card = giftCard
-
-        batch.addTransaction(deactivate)
-
-        load = litleXmlFields.load()
-        load.reportGroup = 'Planets'
-        load.orderId = 'ghi'
-        load.amount = 100
-        load.orderSource = 'ecommerce'
-        load.card = giftCard
-
-        batch.addTransaction(load)
-
-        unload = litleXmlFields.unload()
-        unload.reportGroup = 'Planets'
-        unload.orderId = 'jkl'
-        unload.amount = 100
-        unload.orderSource = 'ecommerce'
-        unload.card = giftCard
-
-        batch.addTransaction(unload)
-
-        balanceInquiry = litleXmlFields.balanceInquiry()
-        balanceInquiry.reportGroup = 'Planets'
-        balanceInquiry.orderId = 'mno'
-        balanceInquiry.orderSource = 'ecommerce'
-        balanceInquiry.card = giftCard
-
-        batch.addTransaction(balanceInquiry)
-
-        fileResponse = request.sendRequestTCP()
-        batchResponse = fileResponse.getNextBatchResponse()
-
-        txns = 0
-
-        nextTransaction = True
-        while nextTransaction:
-            try:
-                txn = batchResponse.getNextTransaction()
-                self.assertNotEqual(None, txn.litleTxnId)
-                txns += 1
-            except NoTransactionException:
-                nextTransaction = False
-
-        self.assertEqual(5, txns)
-
-    def testMechaBatchAndProcess_Recurring(self):
-        requestFileName = "litleSdk-testBatchFile-RECURRING.xml"
-        request = litleBatchFileRequest(requestFileName)
-        configFromFile = request.config
-        self.assertEqual('prelive.litle.com', configFromFile.batchHost)
-        self.assertEqual('15000', configFromFile.batchPort)
-
-        batch = request.createBatch()
-        cancelSubscription = litleXmlFields.cancelSubscription()
-        cancelSubscription.subscriptionId = 12345
-        batch.addTransaction(cancelSubscription)
-
-        updateSubscription = litleXmlFields.updateSubscription()
-        updateSubscription.subscriptionId = 12345
-        batch.addTransaction(updateSubscription)
-
-        createPlan = litleXmlFields.createPlan()
-        createPlan.planCode =  'abc'
-        createPlan.name = 'name'
-        createPlan.intervalType = 'ANNUAL'
-        createPlan.amount = 100
-        batch.addTransaction(createPlan)
-
-        updatePlan = litleXmlFields.updatePlan()
-        updatePlan.planCode = 'def'
-        updatePlan.active = True
-        batch.addTransaction(updatePlan)
-
-        fileResponse = request.sendRequestTCP()
-        batchResponse = fileResponse.getNextBatchResponse()
-
-        txns = 0
-
-        nextTransaction = True
-        while nextTransaction:
-            try:
-                txn = batchResponse.getNextTransaction()
-                if isinstance(txn, litleXmlFields.updateSubscriptionResponse.typeDefinition()):
-                    self.assertEqual(12345, txn.subscriptionId)
-                elif isinstance(txn, litleXmlFields.cancelSubscriptionResponse.typeDefinition()):
-                    self.assertEqual(12345, txn.subscriptionId)
-                elif isinstance(txn, litleXmlFields.createPlanResponse.typeDefinition()):
-                    self.assertEqual('abc', txn.planCode)
-                elif isinstance(txn, litleXmlFields.createPlanResponse.typeDefinition()):
-                    self.assertEqual('def', txn.planCode)
-
-                txns += 1
-            except NoTransactionException:
-                nextTransaction = False
-
-        self.assertEqual(4, txns)
-
-    def testBatch_AU(self):
-        requestFileName = "litleSdk-testBatchFile-AU.xml"
-        request = litleBatchFileRequest(requestFileName)
-        configFromFile = request.config
-        self.assertEqual('prelive.litle.com', configFromFile.batchHost)
-        self.assertEqual('15000', configFromFile.batchPort)
-        batch = request.createBatch()
-
-        #card
-        card = litleXmlFields.card()
-        card.number = '4100000000000001'
-        card.expDate = '1210'
-        card.type = 'VI'
-
-        accountUpdate = litleXmlFields.accountUpdate()
-        accountUpdate.reportGroup = 'Planets'
-        accountUpdate.id = '12345'
-        accountUpdate.customerId = '0987'
-        accountUpdate.orderId = '1234'
-        accountUpdate.cardOrToken = card
-
-        batch.addTransaction(accountUpdate)
-
-        fileResponse = request.sendRequestTCP()
-        batchResponse = fileResponse.getNextBatchResponse()
-
-        txns = 0
-
-        nextTransaction = True
-
-        while nextTransaction:
-            try:
-                txn = batchResponse.getNextTransaction()
-                self.assertEqual('Planets', txn.reportGroup)
-                self.assertEqual('12345', txn.id)
-                self.assertEqual('0987', txn.customerId)
-                self.assertEqual('1234', txn.orderId)
-                txns += 1
-            except NoTransactionException:
-                nextTransaction = False
-
-        self.assertEqual(1, txns)
 
     def assertPythonApi(self, request, response):
         self.assertNotEqual(None, response)
