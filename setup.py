fc1c72913e64f709905490459aed2858bf75385e
Juncai <cai.jun@husky.neu.edu>
2015-04-01 14:08:29 -0400

    Update version to 8.29.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 setup(name='LitleSdkPython',
-      version='8.29.0',
+      version='8.29.2',
       description='Litle & Co. SDK for Python',
       author='Litle & Co',
       author_email='SDKSupport@litle.com',
