8d07031da0525af538e3abb264f0e0f266d75c70
Zhilong Hou <zhilong.hou@vantiv.com>
2017-02-03 14:15:25 -0500

    Rebranding to Vantiv eCommerce

--- litleSdkPythonTest/functional/TestUtf8.py
+++ litleSdkPythonTest/functional/TestUtf8.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-#Copyright (c) 2011-2012 Litle & Co.
+#Copyright (c) 2017 Vantiv eCommerce
 #
 #Permission is hereby granted, free of charge, to any person
 #obtaining a copy of this software and associated documentation
